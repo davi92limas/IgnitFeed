68da1f785c8eda7e9565b0a9d281311afd05afba
davi92limas <57378522+davi92limas@users.noreply.github.com>
2023-12-11 17:00:25 -0300

    Add files via upload

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { Avatar } from './Avatar';
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1569748130764-3fed0c102c59?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      />
+
+      <div className={styles.profile}>
+        <Avatar className={styles.avatar} src="https://github.com/davi92limas.png" />
+        <strong>Davi Limas</strong>
+        <span>Tentando ser um Dev</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
